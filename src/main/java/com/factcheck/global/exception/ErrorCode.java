3c358a256ebfa9085881f66a949d23b3fefb4c13
dnr <rjsdnr1221@naver.com>
2026-04-09 14:59:52 +0900

    feat: 유튜브 댓글 관련도순 조회

--- src/main/java/com/factcheck/global/exception/ErrorCode.java
+++ src/main/java/com/factcheck/global/exception/ErrorCode.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON_001", "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_999", "서버 내부 오류입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_999", "서버 내부 오류입니다."),
+
+    INVALID_YOUTUBE_URL(HttpStatus.BAD_REQUEST, "YOUTUBE_001", "유효하지 않은 유튜브 URL입니다."),
+    UNSUPPORTED_YOUTUBE_URL_FORMAT(HttpStatus.BAD_REQUEST, "YOUTUBE_002", "지원하지 않는 유튜브 URL 형식입니다."),
+    VIDEO_ID_NOT_FOUND(HttpStatus.BAD_REQUEST, "YOUTUBE_003", "videoId를 찾을 수 없습니다."),
+    INVALID_VIDEO_ID(HttpStatus.BAD_REQUEST, "YOUTUBE_004", "유효하지 않은 videoId입니다.");
 
     private HttpStatus status;
     private String code;
